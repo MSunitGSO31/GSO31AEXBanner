6ee5ca4b1b7a0f0d8498de7faf322cf32cbb08e7
MartijnvDijk <martijn.van.dijk@live.nl>
2014-11-03 15:44:44 +0100

    RMI Server

--- src/rmi/RMIServer.java
+++ src/rmi/RMIServer.java
@@ -1,15 +1,74 @@
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
+ * To change this template, choose Tools | Templates
  * and open the template in the editor.
  */
-
 package rmi;
 
+import aexbanner.MockEffectenbeurs;
+import java.rmi.RemoteException;
+import java.rmi.registry.LocateRegistry;
+import java.rmi.registry.Registry;
+
 /**
- *
- * @author Sam
+ * @author Martijn
  */
 public class RMIServer {
-    
+
+    // Set port number
+    private static final int portNumber = 1099;
+
+    // Set binding name for MockEffectenbeurs
+    private static final String bindingName = "mockEffectenBeurs";
+
+    // References to registry and MockEffectenbeurs
+    private Registry registry = null;
+    private MockEffectenbeurs meb = null;
+
+    // Constructor
+    public RMIServer() {
+
+        // Print port number for registry
+        System.out.println("Server: Port number " + portNumber);
+
+        // Create MockEffectenbeurs
+        try {
+            meb = new MockEffectenbeurs();
+            System.out.println("Server: Student administration created");
+        } catch (RemoteException ex) {
+            System.out.println("Server: Cannot create student administration");
+            System.out.println("Server: RemoteException: " + ex.getMessage());
+            meb = null;
+        }
+
+        // Create registry at port number
+        try {
+            registry = LocateRegistry.createRegistry(portNumber);
+            System.out.println("Server: Registry created on port number " + portNumber);
+        } catch (RemoteException ex) {
+            System.out.println("Server: Cannot create registry");
+            System.out.println("Server: RemoteException: " + ex.getMessage());
+            registry = null;
+        }
+
+        // Bind MockEffectenbeurs using registry
+        try {
+            registry.rebind(bindingName, meb);
+        } catch (RemoteException ex) {
+            System.out.println("Server: Cannot bind student administration");
+            System.out.println("Server: RemoteException: " + ex.getMessage());
+        }
+    }
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+
+        // Welcome message
+        System.out.println("SERVER USING REGISTRY");
+
+        // Create server
+        RMIServer server = new RMIServer();
+    }
 }
+
